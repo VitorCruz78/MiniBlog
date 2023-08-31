5968bee2f72a053819ee933dee5d9cfd55cc3ba0
Vitor Luiz da Cruz <vitorluiz15012006@gmail.com>
2023-08-30 23:01:10 -0300

    Inicializando firebase ao projeto

---
+++ miniblog/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVLsalBEn6b_UUK7si6xITxwkwvTCVRR8",
+    authDomain: "miniblog-d9d61.firebaseapp.com",
+    projectId: "miniblog-d9d61",
+    storageBucket: "miniblog-d9d61.appspot.com",
+    messagingSenderId: "22251666502",
+    appId: "1:22251666502:web:4ce67c14e2a8e6dc091e47"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
